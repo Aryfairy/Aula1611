ca126f87f6b4ac700a7bc6c17589a6b079918b27
Aryfairy <ryllarygfs@gmail.com>
2023-11-16 22:10:19 -0300

    tab att

--- app/src/main/java/br/com/etecia/myapp/TabExploreFragment.java
+++ app/src/main/java/br/com/etecia/myapp/TabExploreFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TripsFragment extends Fragment {
+public class TabExploreFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
